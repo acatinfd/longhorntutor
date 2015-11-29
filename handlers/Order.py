b2ff073fdbe1de5b6741f2b1440eb0b5d50397e2
acatinfd <xiaorong@utexas.edu>
2015-11-28 23:29:53 -0800

    Display candidates, select candidate, archived order

--- handlers/Order.py
+++ handlers/Order.py
@@ -9,6 +9,7 @@ from domain.OrderUser import OrderUser
 
 class Order(webapp2.RequestHandler):
     def get(self):
+        #TODO: take care of invalid order_id
         user = users.get_current_user()
         if user:
             order_id = self.request.get('order_id')
@@ -18,10 +19,17 @@ class Order(webapp2.RequestHandler):
             order_info['user_id'] = user.user_id()
 
             order_info['is_applied'] = False
+            order_info['hasSelectedTutor'] = False
+            order_info['own_the_order'] = False
+
             exist_order = OrderUser.query_by_user_and_order(user.user_id(), order_id)
             if (exist_order and exist_order.status_code == 2):
                 order_info['is_applied'] = True
-            #TODO: take care of invalid order_id
+
+            if order_info['selectedTutor']:
+                order_info['hasSelectedTutor'] = True
+                order_info['own_the_order'] = (order_info['selectedTutor']['user_id'] == user.user_id()) or (user.user_id() == order_info['owner_id'])
+
             template = JINJA_ENVIRONMENT.get_template('order.html')
             self.response.write(template.render(order_info))
         else:
--- handlers/api/GetOrderInfo.py
+++ handlers/api/GetOrderInfo.py
@@ -2,6 +2,9 @@ import webapp2
 import json
 from domain.Order import Order
 from domain.UserInfo import UserInfo
+from domain.OrderUser import OrderUser
+from handlers.helper.GetPath import GetPath
+import requests
 
 class GetOrderInfo(webapp2.RequestHandler):
     def get(self):
@@ -15,8 +18,33 @@ class GetOrderInfo(webapp2.RequestHandler):
             status = "open"
             if (order.status_code == 0):
                 status = "close"
+
+            base = GetPath(self.request.url, self.request.path)
+
+            #check if a tutor is selected
+            candidates = []
+            selectedTutor = {}
+
+            all_tutors = OrderUser.query_by_order_id(order_id)
+            tutors = [ {'user_id':ou.user_id } for ou in all_tutors if ou.status_code == 3]
+
+            if len(tutors):
+                selectedTutor['user_id'] = tutors[0]['user_id']
+                r = requests.get(base + '/api/getuser', params={'user_id': selectedTutor['user_id']}).json()
+                selectedTutor['name'] = r['name']
+                selectedTutor['tutor_rating'] = int(r['tutor_rating'] + 0.5)
+                selectedTutor['intro'] = r['intro']
+            else:
+                candidates = [ {'user_id':ou.user_id } for ou in all_tutors if ou.status_code == 2]
+                for p in candidates:
+                    r = requests.get(base + '/api/getuser', params={'user_id': p['user_id']}).json()
+                    p['name'] = r['name']
+                    p['tutor_rating'] = int(r['tutor_rating'] + 0.5)
+                    p['intro'] = r['intro']
+
             self.response.write(json.dumps({'status_code': 0, 'name':user.name, 'owner_id': order.owner_id, \
                 'email':user.email, 'tutor_rating':user.tutor_rating, \
                 'intro':user.intro, 'subject':order.subject, \
                 'title':order.title, 'comment':order.comment, \
-                'order_id':order_id, 'status_code':status}))
+                'order_id':order_id, 'candidates': candidates, \
+                'selectedTutor':selectedTutor, 'status_code':status}))
